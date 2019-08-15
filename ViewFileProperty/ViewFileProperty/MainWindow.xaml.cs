3f3aafab9c6f581b6b472aef562e649590804dc7
Masanori Tanaka <masanorhythm+GitHub@gmail.com>
2019-08-15 18:56:18 +0900

    ［新規］とりあえず作ってみた
    MP3ファイルのプロパティを表示するため、リハビリも兼ねてスタート。

---
+++ ViewFileProperty/ViewFileProperty/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ViewFileProperty
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Drop(object sender, System.Windows.DragEventArgs e)
+        {
+
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void VFP_TextBlock1_Drop(object sender, System.Windows.DragEventArgs e)
+        {
+        }
+    }
+}
